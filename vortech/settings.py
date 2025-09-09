e0215ebbe33d06586867c9cc260a679ce98e3e8b
benitobapela <benitobapela@gmail.com>
2025-09-09 20:49:47 +0100

    mise a jour ALLOWED_HOSTS pour pythonanywhere

--- vortech/settings.py
+++ vortech/settings.py
@@ -132,7 +132,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR / 'media') 
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
