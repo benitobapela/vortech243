319d72e7aa99743701363f4813de8f8f335b6cdc
benitobapela <benitobapela@gmail.com>
2025-09-12 17:52:35 +0100

    lqst commit

--- vortech/urls.py
+++ vortech/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 from .views import *
 from django.conf import settings
 from django.conf.urls.static import static
@@ -17,3 +17,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
