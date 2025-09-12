704a4b2fc7981558c7b10221934ea432192e8e13
benitobapela <benitobapela@gmail.com>
2025-09-12 13:42:48 +0100

    lqst commit

--- vortech/settings.py
+++ vortech/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,6 +55,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'vortech.urls'
+
 
 TEMPLATES = [
     {
@@ -120,10 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "startup/static"
-    
-]
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
